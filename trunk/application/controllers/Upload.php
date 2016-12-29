3492f2c79e815450d362c842d1f07dfc88d570ff
suvison <739585621@qq.com>
2016-12-30 00:42:52 +0800

    b

--- trunk/application/controllers/Upload.php
+++ trunk/application/controllers/Upload.php
@@ -248,10 +248,8 @@ Class Upload extends CI_Controller{
         $image_name = $image_res[1];
         $image_size = $image_res[2]; 
         $image_data = $image_res[3]; 
-        p(explode('.',$image_name));
-        p(array_pop(explode('.',$image_name)));
-        exit();
-        $image_extension = array_pop(explode('.',$image_name));
+        $image_name_arr = explode('.',$image_name);
+        $image_extension = array_pop($image_name_arr);
 
         //构造数据
         $image_info = array(
